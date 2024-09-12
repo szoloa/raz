af40b6871af6c371ccf318fe30ff438cf3e42f3e
szoloa <szoloa@hotmail.com>
2024-09-12 22:07:02 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import random
 import webbrowser
+from tkinter import Listbox
 from tkinter import *
 from tkinter import messagebox
 import tkinter as tk
 import tkinter.filedialog
 import requests
+from tkinter import ttk
+import sv_ttk
+import darkdetect
 
 f = open(r'.\data.txt', encoding='utf-8')
 global theme
@@ -37,15 +41,19 @@ class inputW(tk.Frame):
         self.createFrame()
 
     def createFrame(self):
+        style = ttk.Style()
+        style.configure('TButton', font=("黑体", 11))
+        self.label_web = Label()
+
         self.v1 = StringVar()
-        self.entry_web = Entry(self, textvariable=self.v1)
+        self.entry_web = ttk.Entry(self, textvariable=self.v1, width=45)
         
-        self.button_right = Button(self, text='确定', command=self.changeWeb)
+        self.button_right = ttk.Button(self, text='确定', command=self.changeWeb)
         
-        self.lsb = Listbox(self) 
+        self.lsb = Listbox(self, font=("黑体", 12), width=36) 
         for item in reversed(web_dict):
             self.lsb.insert(0,item)
-        self.button_write = Button(self, text='填入', command=self.list_write)
+        self.button_write = ttk.Button(self, text='填入', command=self.list_write)
 
         self.entry_web.pack()
         self.button_right.pack()
@@ -100,20 +108,21 @@ class UserItem(tk.Frame):
         self.createFrame()
 
     def createFrame(self):
-        self.button_userFile = Button(self, text='选择配置文件', command=self.userDo)
-        self.lsb = Listbox(self) 
+        style = ttk.Style()
+        style.configure('TButton', font=("黑体", 11))
+        self.button_userFile = ttk.Button(self, text='选择配置文件', command=self.userDo)
+        self.lsb = Listbox(self, font=("黑体", 12), width=36) 
 
-        self.entry_item = Entry(self)
+        self.entry_item = ttk.Entry(self, width=45)
         for i in theme[:20]:
             self.lsb.insert(0, i)
 
-        self.button_add = Button(self, text='添加', command=self.addItem)
-        self.button_del = Button(self, text='删除', command=self.delItem)
-        self.button_delall = Button(self, text='清空', command=self.delAllItem)
+        self.button_add = ttk.Button(self, text='添加', command=self.addItem)
+        self.button_del = ttk.Button(self, text='删除', command=self.delItem)
+        self.button_delall = ttk.Button(self, text='清空', command=self.delAllItem)
 
         self.lsb.pack()
         self.button_userFile.pack()
-
 
         self.entry_item.pack()
         self.button_add.pack()
@@ -161,44 +170,51 @@ class appliction(tk.Frame):
         self.createFrame()
 
     def createFrame(self):
-        self.button_rand = Button(self)
+        style = ttk.Style()
+        style.configure('TButton', font=("黑体", 11))
+        self.button_rand = ttk.Button(self)
         self.button_rand['text'] = '随机'
+        self.button_rand['width'] = 64
         self.button_rand['command'] = self.chioceTheme
-
-        self.button_rand_n = Button(self)
+        
+        self.button_rand_n = ttk.Button(self)
         self.button_rand_n['text'] = '仅随机而不跳转'
+        self.button_rand_n['width'] = 16
         self.button_rand_n['command'] = self.chioceThemeNotGo
 
-        self.button_goto = Button(self)
+        self.button_goto = ttk.Button(self)
         self.button_goto['text'] = '选择'
+        self.button_goto['width'] = 64
         self.button_goto['command'] = self.openHistoryPage
         
-        self.button_muti = Button(self)
+        self.button_muti = ttk.Button(self)
         self.button_muti['text'] = '随机多个'
+        self.button_muti['width'] = 6
         self.button_muti['command'] = self.openMuti
 
-        self.button_front = Button(self)
+        self.button_front = ttk.Button(self)
         self.button_front['text'] = '置于最前'
+        self.button_front['width'] = 10
         self.button_front['command'] = self.windFront
 
-        self.button_userDo = Button(self)
+        self.button_userDo = ttk.Button(self)
         self.button_userDo['text'] = '自定义随机文件'
+        self.button_userDo['width'] = 20
         self.button_userDo['command'] = self.userDo
 
-        self.buttoon_changeWeb = Button(self, text='更改搜索引擎', command=self.changeWeb)
+        self.buttoon_changeWeb = ttk.Button(self, text='更改搜索引擎', command=self.changeWeb, width=16)
 
         # 历史记录
-        self.lsb = Listbox(self) 
+        self.lsb = Listbox(self, width=56, font=("黑体", 12)) 
         
-        self.button_rand.pack()
-        self.button_rand_n.pack()
-        self.button_goto.pack()
+        self.button_rand.grid(row=4, column=0, padx=4, pady=4, columnspan=4)
+        self.button_rand_n.grid(row=0, column=0, padx=4, pady=4)
+        self.button_goto.grid(row=3, column=0, padx=4, pady=4, columnspan=4)
         #self.button_muti.pack()
-        self.button_front.pack()
-        self.button_userDo.pack()
-        self.buttoon_changeWeb.pack()
-        self.lsb.pack()
-
+        self.button_front.grid(row=0, column=1, padx=4, pady=4)
+        self.button_userDo.grid(row =0, column=2, padx=4, pady=4)
+        self.buttoon_changeWeb.grid(row=0, column=3, padx=4, pady=4)
+        self.lsb.grid(row=2, column=0, columnspan=4)
 
     def chioceTheme(self):
         chioce = random.choice(theme)
@@ -206,7 +222,10 @@ class appliction(tk.Frame):
         openWeb(chioce)
     
     def openHistoryPage(self):
-        openWeb(self.lsb.get(self.lsb.curselection()))
+        temp = self.lsb.get(self.lsb.curselection())
+        if temp == '':
+            return
+        openWeb(temp)
     
     def openMuti(self, time=5):
         for i in range(time):
@@ -224,7 +243,7 @@ class appliction(tk.Frame):
     # 跳转
     def userDo(self):
         root_web = Tk()
-        root_web.geometry("500x300+200+300")
+        root_web.geometry("500x400+200+300")
         root_web.title("更改随机条目")
         # root_web.iconphoto(True, PhotoImage(file='logo.png'))
         UserItem(master=root_web) 
@@ -232,7 +251,7 @@ class appliction(tk.Frame):
     # 跳转
     def changeWeb(self):
         root_web = Tk()
-        root_web.geometry("500x300+200+300")
+        root_web.geometry("500x400+200+300")
         root_web.title("更改搜索引擎")
         # root_web.iconphoto(True, PhotoImage(file='logo.png'))
         inputW(master=root_web)   
@@ -242,15 +261,27 @@ class appliction(tk.Frame):
         messagebox.showinfo('结果', chioce)
         self.lsb.insert(0, chioce)
 
-root = Tk()
-root.geometry("500x300+100+200")
+root = tk.Tk()
+
+sv_ttk.set_theme(darkdetect.theme())
+
+root.resizable(False, False)
+root.geometry("640x350+200+200")
 root.title("开启随机主题")
 root.iconphoto(True, PhotoImage(file='logo.png'))
 
 root.lift()
-root.attributes('-topmost',True)
-root.after_idle(root.attributes,'-topmost',False)
+root.attributes('-topmost', True)
+root.after_idle(root.attributes, '-topmost', False)
 
 appliction(master=root)
+
+def on_closing():
+    if messagebox.askokcancel("退出", "是否要退出程序？"):
+        root.quit()
+        root.destroy()
+        quit()
+
+root.protocol('WM_DELETE_WINDOW', on_closing)
 
 root.mainloop()
