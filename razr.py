8f2341fe1f93295277e2dd304a7fe08141d8b344
szoloa <szoloa@hotmai.com>
2025-02-15 17:30:34 +0800

    删除：     razr.py

--- razr.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import random
-
-f = open(r'/home/szoloa/temp/raz/data/默认.txt', encoding='utf-8')
-theme = [i.replace('\n','').replace('\r','') for i in f.readlines()]
-f.close()
-
-print(random.choice(theme))
