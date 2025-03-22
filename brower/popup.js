ad2fe5faa58289cd0ad3ad688e2759ebdfe9e187
szoloa <szoloa@hotmai.com>
2025-03-22 22:18:26 +0800

    20250322

--- brower/popup.js
+++ brower/popup.js
@@ -1,10 +1,9 @@
 const fileSelector = document.getElementById("fileSelector");
-// const randomButton = document.getElementById("randomButtonWebsite");
+
 const openButton = document.getElementById("openRandomWebsite");
-// Example file list
+
 const files = ["rurl.txt", "rurlamv_2023.txt", "rurlamv_2025.txt"];
 
-// Populate the dropdown menu
 files.forEach(file => {
   const option = document.createElement("option");
   option.value = file;
@@ -13,17 +12,8 @@ files.forEach(file => {
 });
 
 var index = fileSelector.selectedIndex;
-//获取选中的值
+
 var filename = fileSelector.options[index].value;
-
-// randomButton.addEventListener("click", () => {
-//   const randomFile = files[Math.floor(Math.random() * files.length)];
-//   alert(`Randomly selected file: ${randomFile}`);
-// });
-
-// document.getElementById("openRandomWebsite").addEventListener("click", () => {
-//   chrome.runtime.sendMessage({ action: 'openRandomWebsite', params: filename });
-// });
 
 openButton.addEventListener("click", () => {
   const selectedIndex = fileSelector.selectedIndex; // Get current selected index
